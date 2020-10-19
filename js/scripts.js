4e81d87e6669ba8d94708d012d94034350d69273
cordenbrock <cordenbrock@gmail.com>
2020-10-19 14:01:09 -0700

    add methods to record first and last letters of user input
    shannon grantski <72650879+grantskis@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
-const getWords = function() {
+function getWords(firstLetter, secondLetter) {
+    return firstLetter + secondLetter;
+}
+
 const initial = prompt("Give me worrrrrds!!");
-const firstLetter = toUpperCase(initial.substr(0,1));
-const secondLetter = toUpperCase(initial.substr(initial.length-1, initial.length));
-};
+const firstLetter = initial.substr(0,1).toUpperCase();
+const secondLetter = initial.substr(initial.length-1, initial.length).toUpperCase();
 
-getWords();
+const result = getWords(firstLetter, secondLetter);
+console.log(result);
 
-const result = concat(firstLetter, secondLetter);
-alert(result);
+// const revResult1 = result.charAt(1);
+// const revResult2 = result.charAt(0);
+// alert(revResult1 + revResult2);
+const revResult = result.charAt(1) + result.charAt(0);
+console.log(revResult);
